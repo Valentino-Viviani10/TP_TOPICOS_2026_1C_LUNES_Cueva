a841d8263c382e18c6627b8b4254ff8fec1e146e
Valentino-Viviani10 <vtinoviviani@gmail.com>
2026-04-21 12:52:04 -0300

    Creacion del TP

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Apellido: Viviani, Valentino
+    DNI: 46582790
+    Usuario: Valentino-Viviani10
+    Entrega: Sí
+*/
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
