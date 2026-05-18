820df4f14ecb0e24cdd543d48c152a8302ec479e
valentin <nitnelavbestene@gmail.com>
2026-05-18 01:06:23 -0300

    Corrige bug de duplicación del cheat de borrado

--- main.c
+++ main.c
@@ -357,13 +357,6 @@ int main(int argc, char* argv[])
                         eliminar_fila_y_bajar(tablero, indice_fila, columnas);
                     }
                 }
-                //cheat
-                if(!juego_pausado && (tecla >= GBTK_1 && tecla <= GBTK_9)){
-                    int indice_fila = filas - (tecla - '0');
-                    if(indice_fila >= 0) {
-                        eliminar_fila_y_bajar(tablero, indice_fila, columnas);
-                    }
-                }
             }
 
             if(pantalla == 3){
